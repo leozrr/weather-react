f5564bbd90f8929f27c1f2ec614a1326570eac96
Leona <leona.zerr@gmail.com>
2023-05-02 20:18:51 +0200

    added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Zurich" />
         <footer>
           This project is{" "}
           <a href="https://github.com/leozrr/weather-react" target="_blank">
